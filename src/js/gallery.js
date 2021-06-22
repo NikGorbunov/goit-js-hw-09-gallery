13e5a8207159cdf14e2fba30469542049c8dd92b
NikGorbunov <72225015+NikGorbunov@users.noreply.github.com>
2021-06-22 18:28:02 +0300

    hw9

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,49 +1,65 @@
-import galleryItems from "index.js";
-
-const galleryStorageList = document.querySelector("ul.js-gallery");
-const lightboxRef = document.querySelector("div.js-lightbox");
-const onLightboxContentRef = document.querySelector(".lightbox__image");
-const onCloseBtnLightboxRef = document.querySelector(
-  "button[data-action=close-lightbox]"
-);
-
-const methodForCreatMarkup = galleryItems.reduce(
-  (string, gallery) =>
-    (gallery.innerHTML =
-      string +
-      `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${gallery.original}">
-        <img
-          class="gallery__image"
-          src="${gallery.preview}"
-          data-source="${gallery.original}"
-          alt="${gallery.description}"
-        />
-      </a>
-    </li>`),
-  ""
-);
-
-galleryStorageList.insertAdjacentHTML("afterbegin", methodForCreatMarkup);
-
-galleryStorageList.addEventListener("click", modalOpenMethod);
-
-function modalOpenMethod(evt) {
-  evt.preventDefault();
-  if (evt.target.nodeName !== "IMG") {
-    return;
-  }
-
-  lightboxRef.classList.add("is-open");
-  onLightboxContentRef.src = evt.target.dataset.source;
-  onLightboxContentRef.alt = evt.target.alt;
-}
-
-onCloseBtnLightboxRef.addEventListener("click", modalCloseMethod);
-
-function modalCloseMethod() {
-  lightboxRef.classList.remove("is-open");
-  onLightboxContentRef.src = "";
-  onLightboxContentRef.alt = "";
-}
+export default [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
